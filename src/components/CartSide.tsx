be93cd7054a1ffe53d5b7831cad3c6c34ccf2f37
mohammedzeglam-pg <mohammedzeglam@gmail.com>
2023-04-18 00:02:03 +0300

    change to zod

--- src/components/CartSide.tsx
+++ src/components/CartSide.tsx
@@ -1,6 +1,8 @@
 import { useCallback, useEffect, useState } from "react";
 import { proxy, subscribe, useSnapshot } from "valtio";
-
+import { zodI18nMap } from "zod-i18n-map";
+import i18next from "i18next";
+import translation from "zod-i18n-map/locales/ar/zod.json";
 import { MinusIcon, PlusIcon, Trash2Icon, XIcon } from "lucide-react";
 import { Product } from "./Products";
 
@@ -10,10 +12,11 @@ import { Input } from "./Input";
 import { derive } from "valtio/utils";
 import { toast } from "react-toastify";
 import { ShoppingCart } from "lucide-react";
-import { Select, SelectContent, SelectTrigger, SelectValue } from "./Select";
-import { useQuery } from "@tanstack/react-query";
-import { SelectItem } from "@radix-ui/react-select";
 
+import { useQuery } from "@tanstack/react-query";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
 const cartStore = proxy<{ products: any[]; total: number }>(
   JSON.parse(localStorage?.getItem("cart") as string) || {
     products: [],
@@ -100,8 +103,43 @@ function getLocations() {
 function getShipping(id: number) {
   return fetch(`/api/locations/${id}/shipping`).then((r) => r.json());
 }
+
+i18next.init({
+  lng: "ar",
+  resources: {
+    ar: { zod: translation },
+  },
+});
+z.setErrorMap(zodI18nMap);
+const schema = z.object({
+  name: z.string().min(3),
+  email: z.string().email(),
+  phone: z
+    .string()
+    .regex(/^09(1|2|4|5)[0-9]{7}$/, { message: "رقم الهاتف غير صحيح" }),
+  address: z.string().min(5),
+  note: z.string().optional(),
+  location: z.number().int(),
+  shipping: z.number().int(),
+});
 function AddressForm() {
-  const { register, handleSubmit, getValues } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      name: "",
+      email: "",
+      phone: "",
+      address: "",
+      note: "",
+      location: "",
+      shipping: "",
+    },
+    resolver: zodResolver(schema),
+  });
+
   const onSubmit = (data: any) => console.log(data);
   const locations = useQuery(["locations"], getLocations);
   const [selectedLocationID, setSelectedLocationID] = useState(0);
@@ -116,67 +154,59 @@ function AddressForm() {
   return (
     <div className="p-4 bg-primary-500 rounded flex flex-col gap-4 min-w-[20rem]">
       <h3 className="self-center">تفاصيل التسليم</h3>
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <form onSubmit={handleSubmit(onSubmit)} noValidate>
         <label>
           الاسم
-          <Input {...register("name", { required: true })} className="mt-2" />
+          <Input {...register("name")} className="mt-2" />
+          <p className="empty text-red-500">{errors.name?.message}</p>
+        </label>
+        <label>
+          البريد الالكتروني
+          <Input type="email" {...register("email")} className="mt-2" />
+          <p className="empty text-red-500">{errors.email?.message}</p>
+        </label>
+        <label>
+          الهاتف
+          <Input {...register("phone")} className="mt-2" />
+          <p className="empty text-red-500">{errors.phone?.message}</p>
+        </label>
+        <label>
+          العنوان
+          <Input {...register("address")} className="mt-2" />
+          <p className="empty text-red-500">{errors.address?.message}</p>
+        </label>
+        <label>
+          ملاحظات على الطلبية
+          <Input {...register("note")} className="mt-2" />
+          <p className="empty text-red-500">{errors.note?.message}</p>
+        </label>
+        <label>
+          اختر المدينة
+          <select
+            {...register("location")}
+            className="flex h-10 w-full rounded-md border border-slate-300 bg-primary-700 py-2 px-3 text-sm placeholder:text-slate-400 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-700 dark:text-slate-50 dark:focus:ring-slate-400 dark:focus:ring-offset-slate-900"
+          >
+            {locations?.data?.map((location) => (
+              <option value={`${location.id}`} key={location.id}>
+                {location.name}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label>
+          اختر طريقة التوصيل
+          <select
+            {...register("location")}
+            className="flex h-10 w-full rounded-md border border-slate-300 bg-primary-700 py-2 px-3 text-sm placeholder:text-slate-400 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-700 dark:text-slate-50 dark:focus:ring-slate-400 dark:focus:ring-offset-slate-900"
+          >
+            {shipping?.data?.map((shipping) => (
+              <option value={shipping.id} key={shipping.id}>
+                {shipping.name}
+              </option>
+            ))}
+          </select>
         </label>
       </form>
-      <label>
-        البريد الالكتروني
-        <Input
-          type="email"
-          {...register("name", { required: true })}
-          className="mt-2"
-        />
-      </label>
-      <label>
-        الهاتف
-        <Input
-          {...register("name", {
-            required: true,
-            pattern: /^09(1|2|4|5)[0-9]{7}$/,
-          })}
-          className="mt-2"
-        />
-      </label>
-      <label>
-        العنوان
-        <Input {...register("name", { required: true })} className="mt-2" />
-      </label>
-      <label>
-        ملاحظات على الطلبية
-        <Input {...register("name")} className="mt-2" />
-      </label>
-      <label>
-        اختر المدينة
-        <select
-          {...register("location", {
-            required: true,
-          })}
-          className="flex h-10 w-full rounded-md border border-slate-300 bg-primary-700 py-2 px-3 text-sm placeholder:text-slate-400 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-700 dark:text-slate-50 dark:focus:ring-slate-400 dark:focus:ring-offset-slate-900"
-        >
-          {locations?.data?.map((location) => (
-            <option value={location.id} key={location.id}>
-              {location.name}
-            </option>
-          ))}
-        </select>
-      </label>
-      <label>
-        اختر طريقة التوصيل
-        <select
-          {...register("location", { required: true })}
-          className="flex h-10 w-full rounded-md border border-slate-300 bg-primary-700 py-2 px-3 text-sm placeholder:text-slate-400 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-700 dark:text-slate-50 dark:focus:ring-slate-400 dark:focus:ring-offset-slate-900"
-        >
-          <option disabled selected></option>
-          {shipping?.data?.map((shipping) => (
-            <option value={shipping.id} key={shipping.id}>
-              {shipping.name}
-            </option>
-          ))}
-        </select>
-      </label>
     </div>
   );
 }
